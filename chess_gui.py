579bb7a4e4fbf57b04710600dcc3350e3dacb6e5
Bruno Lippert <bruno.lippert@pragma.team>
2022-06-14 16:42:01 -0300

    clean gui

--- chess_gui.py
+++ chess_gui.py
@@ -86,28 +86,6 @@ def highlight_square(screen, game_state, valid_moves, square_selected):
 
 
 def main():
-    # Check for the number of players and the color of the AI
-    human_player = ""
-    while True:
-        try:
-            number_of_players = input("How many players (1 or 2)?\n")
-            if int(number_of_players) == 1:
-                number_of_players = 1
-                while True:
-                    human_player = input("What color do you want to play (w or b)?\n")
-                    if human_player is "w" or human_player is "b":
-                        break
-                    else:
-                        print("Enter w or b.\n")
-                break
-            elif int(number_of_players) == 2:
-                number_of_players = 2
-                break
-            else:
-                print("Enter 1 or 2.\n")
-        except ValueError:
-            print("Enter 1 or 2.")
-
     py.init()
     screen = py.display.set_mode((WIDTH, HEIGHT))
     clock = py.time.Clock()
@@ -119,21 +97,19 @@ def main():
     valid_moves = []
     game_over = False
 
-    ai = ai_engine.chess_ai()
     game_state = chess_engine.game_state()
-    if human_player is 'b':
-        ai_move = ai.minimax_black(game_state, 3, -100000, 100000, True, Player.PLAYER_1)
-        game_state.move_piece(ai_move[0], ai_move[1], True)
 
     while running:
         for e in py.event.get():
             if e.type == py.QUIT:
                 running = False
-            elif e.type == py.MOUSEBUTTONDOWN:
+            elif e.type == py.MOUSEBUTTONDOWN: #Mouse events
                 if not game_over:
                     location = py.mouse.get_pos()
                     col = location[0] // SQ_SIZE
                     row = location[1] // SQ_SIZE
+
+                    # If clicked on the same square, deselect it
                     if square_selected == (row, col):
                         square_selected = ()
                         player_clicks = []
@@ -141,24 +117,17 @@ def main():
                         square_selected = (row, col)
                         player_clicks.append(square_selected)
                     if len(player_clicks) == 2:
-                        # this if is useless right now
+                        # If second click not a valiod move
                         if (player_clicks[1][0], player_clicks[1][1]) not in valid_moves:
                             square_selected = ()
                             player_clicks = []
                             valid_moves = []
-                        else:
+                        else: # Moves the piece
                             game_state.move_piece((player_clicks[0][0], player_clicks[0][1]),
                                                   (player_clicks[1][0], player_clicks[1][1]), False)
                             square_selected = ()
                             player_clicks = []
                             valid_moves = []
-
-                            if human_player is 'w':
-                                ai_move = ai.minimax_white(game_state, 3, -100000, 100000, True, Player.PLAYER_2)
-                                game_state.move_piece(ai_move[0], ai_move[1], True)
-                            elif human_player is 'b':
-                                ai_move = ai.minimax_black(game_state, 3, -100000, 100000, True, Player.PLAYER_1)
-                                game_state.move_piece(ai_move[0], ai_move[1], True)
                     else:
                         valid_moves = game_state.get_valid_moves((row, col))
                         if valid_moves is None:
@@ -190,74 +159,6 @@ def main():
 
         clock.tick(MAX_FPS)
         py.display.flip()
-
-    # elif human_player is 'w':
-    #     ai = ai_engine.chess_ai()
-    #     game_state = chess_engine.game_state()
-    #     valid_moves = []
-    #     while running:
-    #         for e in py.event.get():
-    #             if e.type == py.QUIT:
-    #                 running = False
-    #             elif e.type == py.MOUSEBUTTONDOWN:
-    #                 if not game_over:
-    #                     location = py.mouse.get_pos()
-    #                     col = location[0] // SQ_SIZE
-    #                     row = location[1] // SQ_SIZE
-    #                     if square_selected == (row, col):
-    #                         square_selected = ()
-    #                         player_clicks = []
-    #                     else:
-    #                         square_selected = (row, col)
-    #                         player_clicks.append(square_selected)
-    #                     if len(player_clicks) == 2:
-    #                         if (player_clicks[1][0], player_clicks[1][1]) not in valid_moves:
-    #                             square_selected = ()
-    #                             player_clicks = []
-    #                             valid_moves = []
-    #                         else:
-    #                             game_state.move_piece((player_clicks[0][0], player_clicks[0][1]),
-    #                                                   (player_clicks[1][0], player_clicks[1][1]), False)
-    #                             square_selected = ()
-    #                             player_clicks = []
-    #                             valid_moves = []
-    #
-    #                             ai_move = ai.minimax(game_state, 3, -100000, 100000, True, Player.PLAYER_2)
-    #                             game_state.move_piece(ai_move[0], ai_move[1], True)
-    #                     else:
-    #                         valid_moves = game_state.get_valid_moves((row, col))
-    #                         if valid_moves is None:
-    #                             valid_moves = []
-    #             elif e.type == py.KEYDOWN:
-    #                 if e.key == py.K_r:
-    #                     game_over = False
-    #                     game_state = chess_engine.game_state()
-    #                     valid_moves = []
-    #                     square_selected = ()
-    #                     player_clicks = []
-    #                     valid_moves = []
-    #                 elif e.key == py.K_u:
-    #                     game_state.undo_move()
-    #                     print(len(game_state.move_log))
-    #         draw_game_state(screen, game_state, valid_moves, square_selected)
-    #
-    #         endgame = game_state.checkmate_stalemate_checker()
-    #         if endgame == 0:
-    #             game_over = True
-    #             draw_text(screen, "Black wins.")
-    #         elif endgame == 1:
-    #             game_over = True
-    #             draw_text(screen, "White wins.")
-    #         elif endgame == 2:
-    #             game_over = True
-    #             draw_text(screen, "Stalemate.")
-    #
-    #         clock.tick(MAX_FPS)
-    #         py.display.flip()
-    #
-    # elif human_player is 'b':
-    #     pass
-
 
 def draw_text(screen, text):
     font = py.font.SysFont("Helvitca", 32, True, False)
